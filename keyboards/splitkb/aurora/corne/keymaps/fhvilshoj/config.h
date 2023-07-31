cf7161bf0170a24a44acf7c77a849b7ccb261393
Frederik Hvilshøj <fhvilshoj@gmail.com>
2023-07-31 20:40:56 +0200

    fix: set some constants

--- keyboards/splitkb/aurora/corne/keymaps/fhvilshoj/config.h
+++ keyboards/splitkb/aurora/corne/keymaps/fhvilshoj/config.h
@@ -1,7 +1,7 @@
 #define IGNORE_MOD_TAP_INTERRUPT
-// #define TAPPING_TERM 200  // too slow 2023-01-16 20:57
-#define UNICODE_SELECTED_MODES UNICODE_MODE_MACOS
-#define TAPPING_TERM 190  // 2023-01-17 20:58
+#define TAPPING_TERM 250  // 2023-01-26 21:17 Try to increase it to avoid comand space fuckups
 #define PERMISSIVE_HOLD
 #define TAPPING_FORCE_HOLD
 
+// #define TAPPING_TERM 200  // too slow 2023-01-16 20:57
+// #define TAPPING_TERM 190  // 2023-01-17 20:58
